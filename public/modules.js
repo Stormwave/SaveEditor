c8719eadc63946a8ec6e9af0d9606a351440669d
Andrew Cornforth <andrewcornforth@googlemail.com>
2021-06-26 18:31:04 +0100

    Changed module loading functions

--- public/modules.js
+++ public/modules.js
@@ -1,21 +1,28 @@
 import { TrailsInTheSkyFC } from './modules/trails_in_the_sky_fc/module.js';
 //import { TrailsInTheSkySC } from './modules/trails_in_the_sky_sc/module.js';
 
+let load_modules = 
+[ 
+    { class:TrailsInTheSkyFC, path:"trails_in_the_sky_fc" },
+    //{ class:TrailsInTheSkySC, path:"trails_in_the_sky_sc" } 
+];
 var modules = [];
 
-let count = 2;
 function loadModule(path, Module)
 {
     let mod = new Module();
     mod.setPath("./modules/"+path+"/").then(()=>
     {
         modules.push(mod);
-        if (modules.length==count)
+        if (modules.length==load_modules.length)
         {
             let modules_loaded_event = new CustomEvent("modules_loaded", { "detail":modules });
             document.dispatchEvent(modules_loaded_event);
         }
     })
 }
-loadModule("trails_in_the_sky_fc", TrailsInTheSkyFC);
-//loadModule("trails_in_the_sky_sc", TrailsInTheSkySC);
+
+for (let i=0;i<load_modules.length;i++)
+{
+    loadModule(load_modules[i].path, load_modules[i].class);
+}
